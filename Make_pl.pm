7c439f46e475f8506b39a03cbb364c68c28ba151
quietfanatic <lewis@wall.org>
2013-09-26 21:13:12 -0700

    Use a real license (The MIT license)

--- Make_pl.pm
+++ Make_pl.pm
@@ -1,17 +1,32 @@
 #!/usr/bin/perl
 
- #####
- #
  # Make_pl - Portable drop-in build system
  # https://github.com/quietfanatic/make-pl
- #
- # Use, copy, distribute, or hack all you want.  If you publish a modified
- #  version, record that you've modified it in this header.  Don't remove
- #  or change anything from this header that wasn't written by yourself.
- #
- # Version 2013-07-01
- #
- ##### (end of 'this header')
+ # (Just copy this into your project directory somewhere)
+
+=cut
+The MIT License (MIT)
+
+Copyright (c) 2013 Lewis Wall
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+=cut
 
 package Make_pl;
 
