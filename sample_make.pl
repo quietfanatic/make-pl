d8de626c9cb05db561396590bbdf016f720da009
quietfanatic <lewis@wall.org>
2014-12-16 21:04:23 -0800

    Update tragically out of date sample_make.pl

--- sample_make.pl
+++ sample_make.pl
@@ -1,9 +1,7 @@
 #!/usr/bin/perl
-use strict;
-use warnings;
-use FindBin;
-use if !$^S, lib => "$FindBin::Bin/.";
-use MakePl;
+use lib do {__FILE__ =~ /^(.*)[\/\\]/; ($1||'.')◀path▶};
+use MakePl;  # Automatically imports strict and warnings
+use Cwd 'realpath';
 
  # Sample make.pl
  # https://github.com/quietfanatic/make-pl
@@ -19,11 +17,17 @@ my $program = 'program';
 my $cc = 'gcc -Wall';
 my $cppc = 'g++ -Wall';
 my $ld = 'g++ -Wall';
+my @includes = (cwd);
+
+ # Link all .o files into the final program
+rule $program, sub { grep /\.o$/, targets }, sub {
+    run "$ld @{$_[1]} -o $_[0][0]";
+}
 
  # Register a pair of .c and .o files
 sub module {
     my ($file) = @_;
-    $file =~ /^(.*)\.c(pp)?$/ or die "Filename given to object() wasn't a .c file: $file\n";
+    $file =~ /^(.*)\.c(pp)?$/ or die "Filename given to module() wasn't a .c[pp] file: $file\n";
     my $base = $1;
     my $compiler = $2 ? $cppc : $cc;
     rule "$base.o", $file, sub {
@@ -31,37 +35,30 @@ sub module {
     }
 }
 
- # Let's generate subdependencies from #include statements.  This only scans
- # #includes with quotes (not angle brackets) and they should all be relative
- # to the file doing the including.
-sub scan_includes {
-    use File::Spec::Functions qw(:ALL);
+ # Find all C/C++ files and declare a rule for them
+module($_) for glob '*.c *.cpp */*.c */*.cpp */*/*.c */*/*.cpp';
+
+ # An finally a cleanup rule
+rule 'clean', [], sub {
+    unlink $program, grep /\.o$/, targets;
+}
+
+ # Let's generate subdependencies from #include statements.
+ # This only scans #includes with quotes (not angle brackets).
+subdep sub {
     my ($file) = @_;
+     # Select only C++ files
     $file =~ /\.(?:c|cpp|h)$/ or return ();
-    my @vdf = splitpath($file);
-    my $base = catpath($vdf[0], $vdf[1], '');
-    open my $F, '<', $file or (warn "Could not open $file: $!\n" and return);
-    read $F, my $head, 2048;  # Only bother scanning first 2k
-    close $F;
+    my $base = ($file =~ /(.*?)[^\\\/]*$/ and $1);
+     # Only bother reading first 2k
+    my @incs = (slurp $file, 2048) =~ /^\s*#include\s*"([^"]*)"/gmi;
     my @r;
-    for ($head =~ /^\s*#include\s*"([^"]*)"/gmi) {
-        push @r, rel2abs($_, $base);
+    for (@incs) {
+        for my $I (@includes, $base) {
+            push @r, realpath("$I/$_") if -e("$I/$_");
+        }
     }
     return @r;
 }
 
-workflow {
-
-    rule $program, sub { targetmatch(qr/\.o$/) }, sub {
-        run "$ld @{$_[1]} -o $_[0][0]";
-    }
-
-    module($_) for glob '*.c *.cpp */*.c */*.cpp */*/*.c */*/*.cpp';
-
-    rule 'clean', [], sub {
-        unlink $program, targetmatch(qr/\.o$/);
-    }
-
-    subdep \&scan_includes;
-
-};
+make;
